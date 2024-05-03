8124e067920c3b66c8eba5b1bf54c2096b7d6e84
Eleven <idiotalex@163.com>
2024-05-03 16:26:00 +0800

    fix: ui

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,71 +1,110 @@
-import { Checkbox } from '@radix-ui/react-checkbox'
 import { Button } from '@/components/ui/button'
-import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
+import { Card, CardHeader, CardTitle, CardContent, CardFooter } from '@/components/ui/card'
+import { useNavigate, useLocation } from 'react-router-dom'
 
 export function Home() {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  const onAction = (action: string) => {
+    const path = `/?action=${action}`
+    navigate(path)
+  }
+
   return (
     <>
       <div className="mx-auto grid w-full max-w-6xl gap-2">
         <h1 className="text-3xl font-semibold">Flomo Extension 说明</h1>
       </div>
       <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav
-          className="grid gap-4 text-sm text-muted-foreground" x-chunk="dashboard-04-chunk-0"
-        >
-          <a href="#" className="font-semibold text-primary">Flomo Extension 介绍</a>
-          <a href="#" className="text-primary">安装说明</a>
-          <a href="#" className="text-primary">使用说明</a>
-          <a href="#" className="text-primary">Support</a>
-          <a href="#" className="text-primary">Organizations</a>
-          <a href="#" className="text-primary">Advanced</a>
+        <nav className="grid gap-4 text-sm text-muted-foreground" x-chunk="dashboard-04-chunk-0">
+          <a onClick={() => onAction('instruction')} className="font-semibold text-primary cursor-pointer">Flomo Extension 介绍</a>
+          <a onClick={() => onAction('install')} className="text-primary cursor-pointer">安装说明</a>
+          <a onClick={() => onAction('usage')} className="text-primary cursor-pointer">使用说明</a>
+          <a onClick={() => onAction('sale')} className="text-primary cursor-pointer">售后服务</a>
         </nav>
         <div className="grid gap-6">
-          <Card x-chunk="dashboard-04-chunk-1">
-            <CardHeader>
-              <CardTitle>Store Name</CardTitle>
-              <CardDescription>
-                Used to identify your store in the marketplace.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <Input placeholder="Store Name" />
-              </form>
-            </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-              <Button>Save</Button>
-            </CardFooter>
-          </Card>
-          <Card x-chunk="dashboard-04-chunk-2">
-            <CardHeader>
-              <CardTitle>Plugins Directory</CardTitle>
-              <CardDescription>
-                The directory within your project, in which your plugins are
-                located.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form className="flex flex-col gap-4">
-                <Input
-                  placeholder="Project Name"
-                  defaultValue="/content/plugins"
-                />
-                <div className="flex items-center space-x-2">
-                  <Checkbox id="include" defaultChecked />
-                  <label
-                    htmlFor="include"
-                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                  >
-                    Allow administrators to change the directory.
-                  </label>
+          {
+            location.search.indexOf('instruction') > -1 ?
+            <Card x-chunk="dashboard-04-chunk-1">
+              <CardHeader>
+                <CardTitle>Flomo Extension 是什么？</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-3 mb-4">
+                  <p>
+                    <a href="https://help.flomoapp.com">flomo 浮墨笔记</a>
+                    ，是一款全平台的卡片笔记，聚焦帮你记录更多想法与灵感，以及更好回顾过往记录。
+                  </p>
                 </div>
-              </form>
-            </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-              <Button>Save</Button>
-            </CardFooter>
-          </Card>
+                <div className="grid gap-3 mb-4">
+                  <p>本插件是为了方便在浏览器上使用时可以随时同步到 flomo 平台而生，简而言之，Flomo Extension 是一个在浏览器上记录 flomo 笔记的插件。</p>
+                </div>
+              </CardContent>
+              <CardFooter className="border-t px-6 py-4">
+                <Button></Button>
+              </CardFooter>
+            </Card> : ''
+          }
+          {
+            location.search.indexOf('install') > -1 ?
+            <Card x-chunk="dashboard-04-chunk-2">
+              <CardHeader>
+                <CardTitle>如何安装 Flomo Extension</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-3 mb-4">
+                  <p>本插件目前只能离线安装：</p>
+                  <p>1. 下载插件的安装文件 zip，下载完成后自行解压</p>
+                  <p>2. 打开 Chrome 浏览器，进入 chrome://extensions/</p>
+                  <p>3. 开启页面右上角的 “开发者模式”</p>
+                  <p>4. 点击 “加载已解压的扩展程序” 并选择解压的文件夹</p>
+                  <p>5. 在浏览器的工具栏中点击该插件，即可正常使用</p>
+                </div>
+              </CardContent>
+              <CardFooter className="border-t px-6 py-4"></CardFooter>
+            </Card> : ''
+          }
+          {
+            location.search.indexOf('usage') > -1 ?
+            <Card x-chunk="dashboard-04-chunk-2">
+              <CardHeader>
+                <CardTitle>如何安装 Flomo Extension</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-3 mb-4">
+                  <p>本插件目前只能离线安装：</p>
+                  <p>1. 下载插件的安装文件 zip，下载完成后自行解压</p>
+                  <p>2. 打开 Chrome 浏览器，进入 chrome://extensions/</p>
+                  <p>3. 开启页面右上角的 “开发者模式”</p>
+                  <p>4. 点击 “加载已解压的扩展程序” 并选择解压的文件夹</p>
+                  <p>5. 在浏览器的工具栏中点击该插件，即可正常使用</p>
+                </div>
+              </CardContent>
+              <CardFooter className="border-t px-6 py-4"></CardFooter>
+            </Card> : ''
+          }
+          {
+            location.search.indexOf('sale') > -1 ?
+            <Card x-chunk="dashboard-04-chunk-2">
+              <CardHeader>
+                <CardTitle>售后服务</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <div className="grid gap-3 mb-4">
+                  <p>如果您遇到了解决不了的问题，请扫描添加下面二维码（烦请备注：flomo插件）:</p>
+                  <p>
+                    <img src="/hotstrip-wx.jpg" className="w-60"></img>
+                  </p>
+                </div>
+                <div className="grid gap-3 mb-4">
+                  <p>如果您对该插件不满意:</p>
+                  <p>欢迎您通过<a href="https://txc.qq.com/products/648748"><b>兔小巢</b></a>反馈您的意见，我们会及时跟进处理。</p>
+                </div>
+              </CardContent>
+              <CardFooter className="border-t px-6 py-4"></CardFooter>
+            </Card> : ''
+          }
         </div>
       </div>
     </>
