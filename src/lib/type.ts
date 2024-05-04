d783df3872ac50d460de9dd12abaf42f1667d4b9
Eleven <idiotalex@163.com>
2024-05-04 21:05:15 +0800

    update: version 1.1.0

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -4,4 +4,4 @@ export interface ApiRes {
   data?: any,
 }
 
-export const FLOMO_EXTENSION_FILE_URL = 'https://flomo-extension-r2.idiotalex.com/flomo-extension-1.0.0-chrome.zip'
+export const FLOMO_EXTENSION_FILE_URL = 'https://flomo-extension-r2.idiotalex.com/flomo-extension-1.1.0-chrome.zip'
