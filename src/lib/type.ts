12dfc40fa27aede4983734f80364c5f8be72e4ba
Eleven <idiotalex@163.com>
2024-10-14 12:14:08 +0800

    feat: v1.11.0

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -4,6 +4,6 @@ export interface ApiRes {
   data?: any,
 }
 
-export const FLOMO_EXTENSION_FILE_URL = 'https://flomo-extension-r2.idiotalex.com/flomo-extension-1.10.0-chrome.zip'
+export const FLOMO_EXTENSION_FILE_URL = 'https://flomo-extension-r2.idiotalex.com/flomo-extension-1.11.0-chrome.zip'
 export const FLOMO_EXTENSION_WEB_STORE_URL = 'https://chromewebstore.google.com/detail/flomo-extension/oepgmpdaajlphmdkepgcgchlmnbpmddf'
 export const FLOMO_EXTENSION_WEB_URL = 'https://flomo-extension-pages.idiotalex.com'
