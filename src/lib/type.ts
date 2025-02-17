54239b25b234964f0b57456aea70fce1bd753fdf
Eleven <idiotalex@163.com>
2025-02-17 14:09:41 +0800

    feat: 更新版本号至 1.15.0，并修改相关下载链接

--- src/lib/type.ts
+++ src/lib/type.ts
@@ -4,6 +4,6 @@ export interface ApiRes {
   data?: any,
 }
 
-export const FLOMO_EXTENSION_FILE_URL = 'https://flomo-extension-r2.hotstrips.org/flomo-extension-1.14.0-chrome.zip'
+export const FLOMO_EXTENSION_FILE_URL = 'https://flomo-extension-r2.hotstrips.org/flomo-extension-1.15.0-chrome.zip'
 export const FLOMO_EXTENSION_WEB_STORE_URL = 'https://chromewebstore.google.com/detail/flomo-extension/oepgmpdaajlphmdkepgcgchlmnbpmddf'
 export const FLOMO_EXTENSION_WEB_URL = 'https://flomo-extension-pages.hotstrips.org'
